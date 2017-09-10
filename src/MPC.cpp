bde168f1cf5aa203cc5396e44e689d864856e015
sameersg <Sameer@sameers-macbook.lan>
2017-09-10 23:21:41 +0200

    not done

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -6,8 +6,8 @@
 using CppAD::AD;
 
 // TODO: Set the timestep length and duration
-size_t N = 0;
-double dt = 0;
+size_t N = 10;
+double dt = .1;
 
 // This value assumes the model presented in the classroom is used.
 //
@@ -21,6 +21,19 @@ double dt = 0;
 // This is the length from front to CoG that has a similar radius.
 const double Lf = 2.67;
 
+double ref_cte = 0;
+double ref_epsi = 0;
+double ref_v = 100;
+
+size_t x_start = 0;
+size_t y_start = x_start + N;
+size_t psi_start = y_start +N;
+size_t v_start = psi_start + N;
+size_t cte_start = v_start + N;
+size_t epsi_start = cte_start + N;
+size_t delta_start = epsi_start + N;
+size_t a_start = delta_start + N -1;
+
 class FG_eval {
  public:
   // Fitted polynomial coefficients
@@ -33,6 +46,9 @@ class FG_eval {
     // `fg` a vector of the cost constraints, `vars` is a vector of variable values (state & actuators)
     // NOTE: You'll probably go back and forth between this function and
     // the Solver function below.
+    fg[0] = 0;
+
+    
   }
 };
 
