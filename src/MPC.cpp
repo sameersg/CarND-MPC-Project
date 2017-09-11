c1562ed8562073925468ec6d7b1174723a0d4030
sameersg <Sameer@sameers-macbook.lan>
2017-09-11 09:42:29 +0200

    not done

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -48,7 +48,71 @@ class FG_eval {
     // the Solver function below.
     fg[0] = 0;
 
-    
+    for(int i = 0; i <N i++){
+      fg[0] += 2000*CppAD::pow(vars[cte_start + i] - ref_cte,2);
+      fg[0] += 2000*CppAD::pow(vars[epsi_start + i] - ref_epsi,2);
+      fg[0] += 2000*CppAD::pow(vars[v_start + i] - ref_v,2);
+    }
+
+    for(int i = 0; i <N -1 i++){
+      fg[0] += 5*CppAD::pow(vars[delta_start + i] ,2);
+      fg[0] += 5*CppAD::pow(vars[a_start + i] ,2);
+    }
+
+    for(int i = 0; i <N -2 i++){
+      fg[0] += 200*CppAD::pow(vars[delta_start + i +1] - vars[delta_start + i] ,2);
+      fg[0] += 10*CppAD::pow(vars[a_start + i + 1] - vars[a_start +i] ,2);
+    }
+
+    // Setup Constraints
+  fg[1 + x_start] = vars[x_start];
+  fg[1 + y_start] = vars[y_start];
+  fg[1 + psi_start] = vars[psi_start];
+  fg[1 + v_start] = vars[v_start];
+  fg[1 + cte_start] = vars[cte_start];
+  fg[1 + epsi_start] = vars[epsi_start];
+
+  // The rest of the constraints
+   for (int i = 1; i < N -1; t++) {
+     // The state at time t+1 .
+     AD<double> x1 = vars[x_start + i +1];
+     AD<double> y1 = vars[y_start + i+1];
+     AD<double> psi1 = vars[psi_start + i+1];
+     AD<double> v1 = vars[v_start + i+1];
+     AD<double> cte1 = vars[cte_start + i+1];
+     AD<double> epsi1 = vars[epsi_start + i+1];
+
+
+     AD<double> x0 = vars[x_start + i];
+     AD<double> y0 = vars[y_start + i];
+     AD<double> psi0 = vars[psi_start + i];
+     AD<double> v0 = vars[v_start + i];
+     AD<double> cte0 = vars[cte_start + i];
+     AD<double> epsi0 = vars[epsi_start + i];
+
+     // Only consider the actuation at time t.
+     AD<double> delta0 = vars[delta_start + i];
+     AD<double> a0 = vars[a_start + i];
+
+     AD<double> f0 = coeffs[0] + coeffs[1] * x0 + coeffs[2] * x0 * x0 + coeffs[3]*x0 * x0 *x0;
+     AD<double> psides0 = CppAD::atan(3*coeffs[3]*x0*x0+2*coeffs[2]*x0+coeffs[1]);
+
+     // Here's `x` to get you started.
+     // The idea here is to constraint this value to be 0.
+     fg[1 + x_start + i] = x1 - (x0 + v0 * CppAD::cos(psi0) * dt);
+     fg[1 + y_start + i] = y1 - (y0 + v0 * CppAD::sin(psi0) * dt);
+     fg[1 + psi_start + i] = psi1 - (psi0 + v0 * delta0 / Lf * dt);
+     fg[1 + v_start + i] = v1 - (v0 + a0 * dt);
+     fg[1 + cte_start + i] =  cte1 - ((f0 - y0) + (v0 * CppAD::sin(epsi0) * dt));
+     fg[1 + epsi_start + i] =  epsi1 - ((psi0 - psides0) + v0 * delta0 / Lf * dt);
+   }
+
+
+
+
+
+
+
   }
 };
 
